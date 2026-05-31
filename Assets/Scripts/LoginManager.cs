3efcd9c15121b63d0a698be316571e01fb8be249
Dom <andresfdominguezp@gmail.com>
2026-05-31 15:14:38 -0500

    LogIn
    -Se creo el login de la segunda escena
    -Creacion del script loginmanager

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LoginManager : MonoBehaviour
+{
+    public TMP_InputField usuarioInput;
+    public TMP_InputField passwordInput;
+    public TMP_Text mensajeError;
+
+    public void Login()
+    {
+        string usuario = usuarioInput.text;
+        string password = passwordInput.text;
+
+        if (usuario == "admin" && password == "1234")
+        {
+            SceneManager.LoadScene("04-Scan");
+        }
+        else
+        {
+            mensajeError.text = "Usuario o contraseña incorrectos";
+        }
+    }
+}
