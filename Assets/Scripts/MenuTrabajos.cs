233cfa6a93a4c0c1763f4be64771a8900a7b612b
rjr1019angulo <rjr.angulo19@gmail.com>
2026-06-04 20:37:12 -0500

    Adelanto Proyecto AR

---
+++ Assets/Scripts/MenuTrabajos.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTrabajos : MonoBehaviour
+{
+    public void IrAROV()
+    {
+        SceneManager.LoadScene("03-Rov");
+    }
+
+    public void IrACalvin()
+    {
+        SceneManager.LoadScene("04-Calvin");
+    }
+
+    public void IrASFerreo()
+    {
+        SceneManager.LoadScene("05-SMFerreo");
+    }
+
+    public void IrARobot()
+    {
+        SceneManager.LoadScene("06-Robot");
+    }
+
+    public void IrAEcosistemas()
+    {
+        SceneManager.LoadScene("07-Ecosistemas");
+    }
+
+    public void IrAVart()
+    {
+        SceneManager.LoadScene("08-Vart");
+    }
+}
